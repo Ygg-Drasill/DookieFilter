41e45ec07eb5c2a91341356d3947c32e53649636
alexrefshauge <arefsh22@student.aau.dk>
2025-02-20 11:57:05 +0100

    frame reader

---
+++ visunator/frameReader/stream.go
@@ -0,0 +1,38 @@
+package frameReader
+
+import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"os"
+	"visunator/frame"
+)
+
+type FrameReader struct {
+	b *bufio.Reader
+}
+
+func New(path string) *FrameReader {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &FrameReader{b: bufio.NewReader(f)}
+}
+
+func (fr *FrameReader) Next() *frame.RawFrame {
+	l, _, err := fr.b.ReadLine()
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	newFrame := new(frame.RawFrame)
+	err = json.Unmarshal(l, newFrame)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return newFrame
+}
