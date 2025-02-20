7de3a39d45a68dbe3e350d698ecbbe79de65ba49
alexrefshauge <arefsh22@student.aau.dk>
2025-02-20 11:30:48 +0100

    .

---
+++ visunator/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	vector.DrawFilledCircle(screen, 32, 32, 16, color.White, true)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	data, err := os.ReadFile("../data.jsonl")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(data)
+	fmt.Println("hello world")
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Visunator")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
