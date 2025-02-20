9d540fe94ac14553621c7ad198c5af077af60bfe
alexrefshauge <arefsh22@student.aau.dk>
2025-02-20 14:00:01 +0100

    track players

--- visunator/frameReader/stream.go
+++ visunator/frameReader/stream.go
@@ -3,13 +3,16 @@ package frameReader
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"visunator/frame"
 )
 
 type FrameReader struct {
-	b *bufio.Reader
+	buff *bufio.Reader
+	file *os.File
 }
 
 func New(path string) *FrameReader {
@@ -17,12 +20,19 @@ func New(path string) *FrameReader {
 	if err != nil {
 		log.Fatal(err)
 	}
-	return &FrameReader{b: bufio.NewReader(f)}
+	return &FrameReader{buff: bufio.NewReader(f), file: f}
 }
 
 func (fr *FrameReader) Next() *frame.Frame[frame.DataPlayer] {
-	l, _, err := fr.b.ReadLine()
+	l, _, err := fr.buff.ReadLine()
 	if err != nil {
+		if err == io.EOF {
+			cerr := fr.file.Close()
+			if cerr != nil {
+				log.Fatal(cerr)
+			}
+			return nil
+		}
 		log.Println(err)
 		return nil
 	}
@@ -32,6 +42,11 @@ func (fr *FrameReader) Next() *frame.Frame[frame.DataPlayer] {
 	if err != nil {
 		log.Println(err)
 		return nil
+	}
+
+	if len(newFrame.Data[0].Ball.Xyz) == 0 { //TODO: maybe return signal later :)
+		fmt.Println("hello")
+		return fr.Next()
 	}
 
 	return newFrame
--- visunator/main.go
+++ visunator/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 	"image/color"
 	"log"
+	"time"
+	"visunator/frame"
 	"visunator/frameReader"
 )
 
@@ -13,20 +17,61 @@ const (
 	SCREEN_H = 480
 )
 
+var (
+	RED = color.RGBA{
+		R: 255,
+	}
+	BLUE = color.RGBA{B: 255}
+)
+
 type Game struct {
-	fr   *frameReader.FrameReader
-	ball []float64
+	fr          *frameReader.FrameReader
+	ball        []float64
+	awayPlayers map[string]frame.Player
+	homePlayers map[string]frame.Player
+	time        time.Time
+	done        bool
 }
 
 func (g *Game) Update() error {
-	g.ball = g.fr.Next().Data[0].Ball.Xyz
+	if g.done {
+		return nil
+	}
+	frame := g.fr.Next()
+	if frame == nil {
+		g.done = true
+		return nil
+	}
+	data := frame.Data[0]
+	g.ball = data.Ball.Xyz
+	g.time = time.Unix(0, data.WallClock*int64(time.Millisecond))
+
+	for _, p := range data.AwayPlayers {
+		g.awayPlayers[p.PlayerId] = p
+	}
+
+	for _, p := range data.HomePlayers {
+		g.homePlayers[p.PlayerId] = p
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	x := SCREEN_W/2 + float32(g.ball[0])
 	y := SCREEN_H/2 + float32(g.ball[1])
-	vector.DrawFilledCircle(screen, x, y, 16, color.White, true)
+
+	vector.DrawFilledCircle(screen, x, y, 8, color.White, true)
+
+	for _, p := range g.awayPlayers {
+		px, py := float32(p.Xyz[0]), float32(p.Xyz[1])
+		vector.DrawFilledCircle(screen, px, py, 16, RED, true)
+	}
+
+	if g.done {
+		ebitenutil.DebugPrint(screen, fmt.Sprintf("%s - %s", g.time.String(), "done"))
+	} else {
+		ebitenutil.DebugPrint(screen, g.time.String())
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -37,7 +82,7 @@ func main() {
 	fr := frameReader.New("./raw.jsonl")
 	ebiten.SetWindowSize(SCREEN_W, SCREEN_H)
 	ebiten.SetWindowTitle("Visunator")
-	if err := ebiten.RunGame(&Game{fr: fr}); err != nil {
+	if err := ebiten.RunGame(&Game{fr: fr, done: false, awayPlayers: make(map[string]frame.Player), homePlayers: make(map[string]frame.Player)}); err != nil {
 		log.Fatal(err)
 	}
 }
