a38aa12d3c66e470b533005ee3e1b6f22ec1166e
alexrefshauge <arefsh22@student.aau.dk>
2025-02-26 13:38:58 +0100

    inputs

--- visunator/app/update.go
+++ visunator/app/update.go
@@ -1,9 +1,6 @@
 package app
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
-	"log"
 	"time"
 )
 
@@ -40,39 +37,4 @@ func (g *Game) Update() error {
 
 	g.lastUpdate = tNow
 	return nil
-}
-
-func (g *Game) HandleInputs() {
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		mx, my := ebiten.CursorPosition()
-		if my < g.height/2 {
-			return
-		}
-		frameIndex := int64(float32(mx) / float32(g.width) * float32(g.frameLoader.FrameCount()))
-		err := g.frameLoader.GoToFrame(frameIndex)
-		g.frameIndex = frameIndex
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
-		g.active = !g.active
-	}
-
-	if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
-		g.frameTime++
-	} else if ebiten.IsKeyPressed(ebiten.KeyShift) && ebiten.IsKeyPressed(ebiten.KeyUp) {
-		g.frameTime++
-	}
-
-	if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
-		g.frameTime--
-	} else if ebiten.IsKeyPressed(ebiten.KeyShift) && ebiten.IsKeyPressed(ebiten.KeyDown) {
-		g.frameTime--
-	}
-
-	if g.frameTime < 20 {
-		g.frameTime = 20
-	}
 }
