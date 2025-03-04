06d723c4e352a6ac0439c0d25e914a3fd77af0a6
alexrefshauge <nk02hb@aau.student.dk>
2025-03-05 00:04:13 +0100

    open push pull sockets

--- services/master/collector/types.go
+++ services/master/collector/types.go
@@ -1,31 +1,75 @@
 package collector
 
 import (
-	zmq "github.com/pebbe/zmq4"
-	"log/slog"
-	"sync"
+    zmq "github.com/pebbe/zmq4"
+    "log/slog"
+    "strings"
+    "sync"
 )
 
 type CollectorWorker struct {
-	socketContext *zmq.Context
+    socketContext    *zmq.Context
+    socketListen     *zmq.Socket
+    socketStore      *zmq.Socket
+    socketDistribute *zmq.Socket
 }
 
 func New(options ...func(worker *CollectorWorker)) *CollectorWorker {
-	worker := &CollectorWorker{}
-	for _, opt := range options {
-		opt(worker)
-	}
-	return worker
+    worker := &CollectorWorker{}
+    for _, opt := range options {
+        opt(worker)
+    }
+    return worker
 }
 
 func (w *CollectorWorker) Run(wg *sync.WaitGroup) {
-	wg.Add(1)
-	defer wg.Done()
+    wg.Add(1)
+    defer wg.Done()
+    var err error
+    w.socketListen, err = w.socketContext.NewSocket(zmq.PULL)
+    if err != nil {
+        slog.Error(err.Error())
+    }
 
-	socket, err := w.socketContext.NewSocket(zmq.PUSH)
-	if err != nil {
-		slog.Error(err.Error())
-	}
-	err = socket.Bind("inproc://collector")
-	slog.Error(err.Error())
+    w.socketStore, err = w.socketContext.NewSocket(zmq.PUSH)
+    if err != nil {
+        slog.Error(err.Error())
+    }
+
+    err = w.socketListen.Bind("inproc://collector")
+    if err != nil {
+        slog.Error(err.Error())
+    }
+
+    for {
+        err := w.collectorListen()
+        if err != nil {
+            slog.Error(err.Error())
+        }
+    }
+}
+
+func (w *CollectorWorker) collectorListen() error {
+    topic, err := w.socketListen.Recv(zmq.SNDMORE)
+    if err != nil {
+        return err
+    }
+
+    msg, err := w.socketListen.RecvMessage(0)
+    if err != nil {
+        return err
+    }
+
+    if topic == "frame" {
+        //parse raw frame
+    }
+
+    if topic == "point" {
+        _, err := w.socketStore.SendMessage(0, "point", msg)
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
 }
--- services/master/main.go
+++ services/master/main.go
@@ -12,13 +12,6 @@ func init() {
 	slog.SetDefault(logger.New("master", "DEBUG"))
 }
 
-const (
-	endpointDealSwap    = "deal-swap"
-	endpointDealJump    = "deal-jump"
-	endpointDealClean   = "deal-clean"
-	endpointDealMissing = "deal-missing"
-)
-
 func main() {
 	slog.Info("starting service")
 	socketCtx, err := zmq.NewContext()
