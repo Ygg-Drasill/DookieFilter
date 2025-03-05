4f2e8bb98171c3c2828c15343f60fd07d86d0611
alexrefshauge <nk02hb@aau.student.dk>
2025-03-05 18:41:48 +0100

    commit

--- services/master/detector/detector.go
+++ services/master/detector/detector.go
@@ -1,20 +1,36 @@
 package detector
 
 import (
+    "github.com/Ygg-Drasill/DookieFilter/common/pringleBuffer"
+    "github.com/Ygg-Drasill/DookieFilter/common/types"
     "github.com/Ygg-Drasill/DookieFilter/services/master/worker"
     zmq "github.com/pebbe/zmq4"
+    "strings"
     "sync"
 )
+
+type gameState struct {
+    players  []types.PlayerPosition
+    ball     types.PlayerPosition
+    frameIdx int
+}
+
+func (g gameState) Key() pringleBuffer.Key {
+    return pringleBuffer.Key(g.frameIdx)
+}
 
 type DetectorWorker struct {
     worker.BaseWorker
 
     socketListen *zmq.Socket
+
+    stateBuffer *pringleBuffer.PringleBuffer[gameState]
 }
 
 func New(ctx *zmq.Context, options ...func(worker *DetectorWorker)) *DetectorWorker {
     w := &DetectorWorker{
-        BaseWorker: worker.NewBaseWorker(ctx, "detector"),
+        BaseWorker:  worker.NewBaseWorker(ctx, "detector"),
+        stateBuffer: pringleBuffer.New[gameState](10),
     }
     for _, opt := range options {
         opt(w)
@@ -33,6 +49,11 @@ func (w *DetectorWorker) Run(wg *sync.WaitGroup) {
     }
 
     for {
-
+        topic, _ := w.socketListen.Recv(zmq.SNDMORE)
+        if topic == "playerframe" {
+            message, _ := w.socketListen.RecvMessage(0)
+            players := types.DeserializePlayerPositions(strings.Join(message, ""))
+            w.stateBuffer.Insert(gameState{players: players, frameIdx: players[0].FrameIdx})
+        }
     }
 }
