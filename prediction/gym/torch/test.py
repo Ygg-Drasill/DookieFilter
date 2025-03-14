d0ebebede55a3484f2a155d643ebe32aec3c37dc
alexrefshauge <arefsh22@student.aau.dk>
2025-03-14 13:56:16 +0100

    correct model test

--- prediction/gym/torch/test.py
+++ prediction/gym/torch/test.py
@@ -4,21 +4,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
-import sys
+
+from gym.utils.data import get_features
 from model import PlayerPredictor
-from utils.data import get_features
 
 if __name__ == "__main__":
-    model_path = os.path.abspath("./model.pth")
+    model_path = os.path.abspath("../../model/out/model.pth")
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = PlayerPredictor(device, 3, 32, 4)
-    model.load_state_dict(torch.load(model_path, weights_only=True))
+    model.load_state_dict(torch.load(model_path, weights_only=True, map_location=device))
     model.to(device)
     model.eval()
 
-    dataframe = pd.read_csv("../data/chunk_0.csv")
+    dataframe = pd.read_csv("../data/chunk_0.csv", low_memory=True)
     d: pd.DataFrame
-    player_key = "h_10"
+    player_key = "h_14"
     start, end = 2500, 3500
     prediction_start = 3000
 
@@ -30,7 +30,6 @@ if __name__ == "__main__":
         feature_coords = features.reshape(-1, 2)
         player_truth.append(feature_coords[0])
         player_prediction.append(feature_coords[0])
-        ball.append(feature_coords[1])
 
     sequence = np.array(sequence)
     input_length = 20
@@ -39,10 +38,12 @@ if __name__ == "__main__":
         input_tensor = torch.from_numpy(sequence[i-input_length:i].reshape(1,input_length, 16)).float().to(device)
         out = model(input_tensor).cpu().detach().numpy()
         player_prediction[i] = out.squeeze()
+        if i < len(sequence)-1:
+            sequence[i+1][0] = out.squeeze()[0]
+            sequence[i+1][1] = out.squeeze()[1]
 
     plt.plot(*np.array(player_truth).T)
     plt.plot(*np.array(player_prediction).reshape(end-start, 2).T)
-    plt.plot(*np.array(ball).T)
 
     plt.legend(["truth", "prediction", "ball"])
     plt.show()
