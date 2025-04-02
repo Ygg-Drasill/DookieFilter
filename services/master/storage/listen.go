d951bcf20827d8f135d6722cdc4249d091f90cdc
alexrefshauge <nk02hb@aau.student.dk>
2025-04-02 20:38:25 +0200

    wait for storage worker to be ready

--- services/master/storage/listen.go
+++ services/master/storage/listen.go
@@ -2,10 +2,17 @@ package storage
 
 import (
 	"fmt"
+	"github.com/Ygg-Drasill/DookieFilter/common/pringleBuffer"
 	"github.com/Ygg-Drasill/DookieFilter/common/types"
 	zmq "github.com/pebbe/zmq4"
+	"strconv"
 	"strings"
 	"sync"
+)
+
+const (
+	REQ_PLAYER_FRAME = "playerFrame"
+	REQ_FRAME_RANGE  = "frameRange"
 )
 
 func (w *Worker) listenConsume(wg *sync.WaitGroup) {
@@ -24,8 +31,15 @@ func (w *Worker) listenConsume(wg *sync.WaitGroup) {
 			frame := types.DeserializeFrame(strings.Join(message, ""))
 			for _, player := range frame.Players {
 				w.mutex.Lock()
-				buffer := w.players[player.PlayerId]
+				buffer, ok := w.players[player.PlayerId]
+				if !ok {
+					playerBuffer := *pringleBuffer.New[types.PlayerPosition](w.bufferSize)
+					w.players[player.PlayerId] = playerBuffer
+					buffer = playerBuffer
+				}
+
 				buffer.Insert(player)
+				w.players[player.PlayerId] = buffer
 				w.mutex.Unlock()
 			}
 		}
@@ -42,7 +56,7 @@ func (w *Worker) listenProvide(wg *sync.WaitGroup) {
 		}
 
 		if topic == "playerFrame" {
-			w.receivePlayerFrameRequest()
+			w.handlePlayerFrameRequest()
 		}
 
 		if topic == "playerNumber" {
@@ -50,29 +64,56 @@ func (w *Worker) listenProvide(wg *sync.WaitGroup) {
 		}
 
 		if topic == "frameRange" {
-			w.receiveFrameRangeRequest()
+			w.handleFrameRangeRequest()
 		}
 	}
 }
 
-func (w *Worker) receivePlayerFrameRequest() {
+func (w *Worker) handlePlayerFrameRequest() {
 	messageParts, err := w.socketProvide.RecvMessage(0)
 	if err != nil {
-		w.Logger.Error("Failed to receive request", "type", "playerFrame", "error", err)
+		w.Logger.Error("Failed to receive request", "type", REQ_PLAYER_FRAME, "error", err)
 		return
 	}
 	message := strings.Join(messageParts, "")
 	frameIndexAndPlayerId := strings.Split(message, ":")
-	frameIndex, playerId := frameIndexAndPlayerId[0], frameIndexAndPlayerId[1]
-	w.Logger.Debug("Handling playerFrame request", "frameIndex", frameIndex, "playerId", playerId)
+	frameIndexRaw, playerId := frameIndexAndPlayerId[0], frameIndexAndPlayerId[1]
+	w.Logger.Debug("Handling storage request", "type", REQ_PLAYER_FRAME, "frameIndex", frameIndexRaw, "playerId", playerId)
+
+	frameIndex, err := strconv.Atoi(frameIndexRaw)
+	if err != nil {
+		w.Logger.Error("Failed to convert frame index to int", "error", err)
+		w.respondEmpty()
+		return
+	}
+	playerBuffer := w.players[playerId]
+	position, err := playerBuffer.Get(pringleBuffer.Key(frameIndex))
+	if err != nil {
+		w.Logger.Error("Failed to get position", "frameIndex", frameIndex, "playerId", playerId, "error", err)
+		w.respondEmpty()
+		return
+	}
+
+	response := fmt.Sprintf("%g;%g", position.X, position.Y)
+
+	_, err = w.socketProvide.Send(response, 0)
+	if err != nil {
+		w.Logger.Error("Failed to send message", "type", REQ_PLAYER_FRAME, "error", err)
+		w.respondEmpty()
+		return
+	}
 }
 
-func (w *Worker) receiveFrameRangeRequest() {
+func (w *Worker) handleFrameRangeRequest() {
 	messageParts, err := w.socketProvide.RecvMessage(0)
 	if err != nil {
-		w.Logger.Error("Failed to receive request", "type", "frameRange", "error", err)
+		w.Logger.Error("Failed to receive request", "type", REQ_FRAME_RANGE, "error", err)
 		return
 	}
 	message := strings.Join(messageParts, "")
 	fmt.Println(message) //TODO: implement
+}
+
+func (w *Worker) respondEmpty() {
+	w.socketProvide.Send("", 0)
 }
