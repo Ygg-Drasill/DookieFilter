e49c4f4d369b2ac398c1fae9b6c6130878ad9f30
alexrefshauge <arefsh22@student.aau.dk>
2025-04-04 17:25:28 +0200

    create sockets before connecting

--- services/master/collector/collector_test.go
+++ services/master/collector/collector_test.go
@@ -1,59 +1,60 @@
 package collector
 
 import (
-    "encoding/json"
-    "github.com/Ygg-Drasill/DookieFilter/common/socket/endpoints"
-    "github.com/Ygg-Drasill/DookieFilter/common/testutils"
-    zmq "github.com/pebbe/zmq4"
-    "github.com/stretchr/testify/assert"
-    "sync"
-    "testing"
+	"encoding/json"
+	"github.com/Ygg-Drasill/DookieFilter/common/socket/endpoints"
+	"github.com/Ygg-Drasill/DookieFilter/common/testutils"
+	zmq "github.com/pebbe/zmq4"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
 )
 
 func TestCollector(t *testing.T) {
 
-    wg := &sync.WaitGroup{}
-    ctx, err := zmq.NewContext()
-    if err != nil {
-        panic(err)
-    }
-    worker := New(ctx)
-    go worker.Run(wg)
+	wg := &sync.WaitGroup{}
+	ctx, err := zmq.NewContext()
+	if err != nil {
+		panic(err)
+	}
+	worker := New(ctx)
+	go worker.Run(wg)
 
-    socketCollector, err := zmq.NewSocket(zmq.PUSH)
-    if err != nil {
-        panic(err)
-    }
-    err = socketCollector.Connect(endpoints.InProcessEndpoint(endpoints.COLLECTOR))
-    assert.NoError(t, err)
-    socketStore, err := zmq.NewSocket(zmq.PULL)
-    if err != nil {
-        panic(err)
-    }
-    err = socketStore.Bind(endpoints.InProcessEndpoint(endpoints.STORAGE))
-    assert.NoError(t, err)
-    socketDetector, err := zmq.NewSocket(zmq.PULL)
-    if err != nil {
-        panic(err)
-    }
-    err = socketDetector.Bind(endpoints.InProcessEndpoint(endpoints.DETECTOR))
-    assert.NoError(t, err)
+	socketCollector, err := zmq.NewSocket(zmq.PUSH)
+	if err != nil {
+		panic(err)
+	}
+	socketStore, err := zmq.NewSocket(zmq.PULL)
+	if err != nil {
+		panic(err)
+	}
+	socketDetector, err := zmq.NewSocket(zmq.PULL)
+	if err != nil {
+		panic(err)
+	}
 
-    frame := testutils.RandomFrame(11, 11)
-    data, err := json.Marshal(frame)
-    if err != nil {
-        panic(err)
-    }
-    _, err = socketCollector.Send("frame", zmq.SNDMORE)
-    _, err = socketCollector.SendMessage(string(data))
-    if err != nil {
-        panic(err)
-    }
+	err = socketCollector.Connect(endpoints.InProcessEndpoint(endpoints.COLLECTOR))
+	assert.NoError(t, err)
+	err = socketStore.Bind(endpoints.InProcessEndpoint(endpoints.STORAGE))
+	assert.NoError(t, err)
+	err = socketDetector.Bind(endpoints.InProcessEndpoint(endpoints.DETECTOR))
+	assert.NoError(t, err)
 
-    storeMessage, err := socketStore.RecvMessage(0)
-    assert.NoError(t, err)
-    assert.NotEmpty(t, storeMessage)
-    detectorMessage, err := socketDetector.RecvMessage(0)
-    assert.NoError(t, err)
-    assert.NotEmpty(t, detectorMessage)
+	frame := testutils.RandomFrame(11, 11)
+	data, err := json.Marshal(frame)
+	if err != nil {
+		panic(err)
+	}
+	_, err = socketCollector.Send("frame", zmq.SNDMORE)
+	_, err = socketCollector.SendMessage(string(data))
+	if err != nil {
+		panic(err)
+	}
+
+	storeMessage, err := socketStore.RecvMessage(0)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, storeMessage)
+	detectorMessage, err := socketDetector.RecvMessage(0)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, detectorMessage)
 }
