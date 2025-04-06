50b0726def2318e008e08567069559f3d4238ed2
alexrefshauge <nk02hb@aau.student.dk>
2025-04-06 14:02:55 +0200

    waitgroup

--- services/master/collector/collector_test.go
+++ services/master/collector/collector_test.go
@@ -1,60 +1,80 @@
 package collector
 
 import (
-	"encoding/json"
-	"github.com/Ygg-Drasill/DookieFilter/common/socket/endpoints"
-	"github.com/Ygg-Drasill/DookieFilter/common/testutils"
-	zmq "github.com/pebbe/zmq4"
-	"github.com/stretchr/testify/assert"
-	"sync"
-	"testing"
+    "encoding/json"
+    "github.com/Ygg-Drasill/DookieFilter/common/socket/endpoints"
+    "github.com/Ygg-Drasill/DookieFilter/common/testutils"
+    zmq "github.com/pebbe/zmq4"
+    "github.com/stretchr/testify/assert"
+    "log/slog"
+    "sync"
+    "testing"
 )
 
 func TestCollector(t *testing.T) {
 
-	wg := &sync.WaitGroup{}
-	ctx, err := zmq.NewContext()
-	if err != nil {
-		panic(err)
-	}
-	worker := New(ctx)
-	go worker.Run(wg)
+    wg := &sync.WaitGroup{}
+    ctx, err := zmq.NewContext()
+    ctx.SetIoThreads(8)
+    if err != nil {
+        panic(err)
+    }
+    worker := New(ctx)
+    wg.Add(1)
+    go worker.Run(wg)
 
-	socketCollector, err := zmq.NewSocket(zmq.PUSH)
-	if err != nil {
-		panic(err)
-	}
-	socketStore, err := zmq.NewSocket(zmq.PULL)
-	if err != nil {
-		panic(err)
-	}
-	socketDetector, err := zmq.NewSocket(zmq.PULL)
-	if err != nil {
-		panic(err)
-	}
+    socketCollector, err := zmq.NewSocket(zmq.PUSH)
+    if err != nil {
+        panic(err)
+    }
+    socketStore, err := zmq.NewSocket(zmq.PULL)
+    if err != nil {
+        panic(err)
+    }
+    socketDetector, err := zmq.NewSocket(zmq.PULL)
+    if err != nil {
+        panic(err)
+    }
 
-	err = socketCollector.Connect(endpoints.InProcessEndpoint(endpoints.COLLECTOR))
-	assert.NoError(t, err)
-	err = socketStore.Bind(endpoints.InProcessEndpoint(endpoints.STORAGE))
-	assert.NoError(t, err)
-	err = socketDetector.Bind(endpoints.InProcessEndpoint(endpoints.DETECTOR))
-	assert.NoError(t, err)
+    err = socketCollector.Connect(endpoints.InProcessEndpoint(endpoints.COLLECTOR))
+    assert.NoError(t, err)
+    err = socketStore.Bind(endpoints.InProcessEndpoint(endpoints.STORAGE))
+    assert.NoError(t, err)
+    err = socketDetector.Bind(endpoints.InProcessEndpoint(endpoints.DETECTOR))
+    assert.NoError(t, err)
 
-	frame := testutils.RandomFrame(11, 11)
-	data, err := json.Marshal(frame)
-	if err != nil {
-		panic(err)
-	}
-	_, err = socketCollector.Send("frame", zmq.SNDMORE)
-	_, err = socketCollector.SendMessage(string(data))
-	if err != nil {
-		panic(err)
-	}
+    frame := testutils.RandomFrame(11, 11)
+    _, err = json.Marshal(frame)
+    if err != nil {
+        panic(err)
+    }
+    go func() {
+        n, err := socketCollector.SendMessage("frame")
+        assert.Greater(t, n, 0)
+        if err != nil {
+            panic(err)
+        }
+    }()
 
-	storeMessage, err := socketStore.RecvMessage(0)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, storeMessage)
-	detectorMessage, err := socketDetector.RecvMessage(0)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, detectorMessage)
+    //Wrapping this part of the test in a separate goroutine
+    //allows the zmq sockets to run in parallel.
+    //Otherwise, socketStore.RecvMessage halts execution for the sockets
+    //inside the worker, which would not be able to receive messages.
+    //(Running it in a t.Run() with t.Parallel() did not work...)
+    testWg := &sync.WaitGroup{}
+    var storeMessage []string
+    var detectorMessage []string
+    testWg.Add(1)
+    go func() {
+        storeMessage, err = socketStore.RecvMessage(0)
+        assert.NoError(t, err)
+        slog.Info("hello")
+
+        detectorMessage, err = socketDetector.RecvMessage(0)
+        assert.NoError(t, err)
+        testWg.Done()
+    }()
+    testWg.Wait()
+    assert.NotEmpty(t, storeMessage)
+    assert.NotEmpty(t, detectorMessage)
 }
