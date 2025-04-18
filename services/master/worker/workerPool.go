b44196985cffab5de7df1e02aedfc7eb8ddb9c91
alexrefshauge <nk02hb@aau.student.dk>
2025-04-18 12:07:52 +0200

    pool refactor

--- services/master/worker/workerPool.go
+++ services/master/worker/workerPool.go
@@ -1,27 +1,27 @@
 package worker
 
 import (
-	"sync"
+    "sync"
 )
 
-type workerPool struct {
-	workers   []Worker
-	waitGroup sync.WaitGroup
+type Pool struct {
+    workers   []Worker
+    waitGroup sync.WaitGroup
 }
 
-func NewPool() *workerPool {
-	return &workerPool{
-		workers:   make([]Worker, 0),
-		waitGroup: sync.WaitGroup{},
-	}
+func NewPool() *Pool {
+    return &Pool{
+        workers:   make([]Worker, 0),
+        waitGroup: sync.WaitGroup{},
+    }
 }
 
-func (pool *workerPool) Add(worker Worker) {
-	pool.waitGroup.Add(1)
-	pool.workers = append(pool.workers, worker)
-	go worker.Run(&pool.waitGroup)
+func (pool *Pool) Add(worker Worker) {
+    pool.waitGroup.Add(1)
+    pool.workers = append(pool.workers, worker)
+    go worker.Run(&pool.waitGroup)
 }
 
-func (pool *workerPool) Wait() {
-	pool.waitGroup.Wait()
+func (pool *Pool) Wait() {
+    pool.waitGroup.Wait()
 }
