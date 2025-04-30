15ae02fa08072041007f51c1dbb2d97801258eb9
alexrefshauge <nk02hb@aau.student.dk>
2025-04-30 23:06:12 +0200

    output delta

--- prediction/gym/train/train.py
+++ prediction/gym/train/train.py
@@ -19,24 +19,59 @@ if __name__ == '__main__':
     batch_size = 64
     n_nearest_players = 3
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    sequence_length = 20
-    chunk_path = os.path.abspath("../data/chunk_0.csv")
-    player_numbers = ["h_10", "h_13", "h_14", "h_2", "h_8", "a_1", "a_3", "a_19", "a_26", "a_44"]
-    player_dataset = ConcatDataset([PlayerDataset(chunk_path, sequence_length, num, 3) for num in player_numbers])
-    train_size = math.floor(len(player_dataset)*dataset_split_ratio)
+    chunk_path = os.path.abspath("../data/test") #use all matches later
+
+    # hyper_parameters = {
+    #     'n_nearest_players': range(1, 5 + 1),
+    #     'stack_size': range(1, 16 + 1),
+    #     'hidden_size': [8, 16, 32, 64, 128, 256, 512],
+    #     'sequence_length': [10, 20, 30, 40],
+    #     'batch_size': [16, 32, 64, 128, 256],
+    #     'lr': [0.0001, 0.00001, 0.1, 0.001],
+    # }
+
+    hyper_parameters = {
+        'n_nearest_players': [5], #3-5
+        'stack_size': [4], #4-32
+        'hidden_size': [64], #32-128
+        'sequence_length': [20], #20-40
+        'batch_size': [64],
+        'lr': [0.0001], #0.0001-0.00001
+    }
+
+if __name__ == '__init__':
+    init()
+
+def train_model(
+    n_nearest: int,
+    stack_size: int,
+    hidden_size: int,
+    sequence_length: int,
+    batch_size: int,
+    lr: float
+):
+    print(f'n:{n_nearest} stack_size:{stack_size} hidden_size:{hidden_size} seq:{sequence_length} lr:{lr} batch_size:{batch_size}')
+    chunk_files = os.listdir(chunk_path)
+    chunk_sizes = [pd.read_csv(f"{chunk_path}/{p}").shape[0] for p in chunk_files]
+    total_samples = sum(chunk_sizes)
+    average_chunk_size = total_samples // len(chunk_sizes)
+    print(f"found {len(chunk_sizes)} chunks of {total_samples} samples with average chunk size {average_chunk_size}")
+
+    player_dataset = ConcatDataset(
+        [PlayerDataset(f"{chunk_path}/{path}", sequence_length, n_nearest) for path in tqdm(os.listdir(chunk_path),
+                                                                                            unit='chunk',
+                                                                                            desc='building dataset from chunks',
+                                                                                            ncols=200)])
+    train_size = math.floor(len(player_dataset) * dataset_split_ratio)
     validation_size = len(player_dataset) - train_size
-    train_set, validation_set = torch.utils.data.random_split(player_dataset, [train_size, validation_size])
-
-
-    #dataset = MatchDataset(os.path.abspath("../data/chunk_0.csv"), device, sequence_length, n_nearest_players=n_nearest_players)
-    train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=8)
-    validation_dataloader = DataLoader(validation_set, batch_size=batch_size, shuffle=True, num_workers=8)
+    train_set, validation_set = torch.utils.data.random_split(player_dataset,[train_size, validation_size])
+    train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=1)
+    validation_dataloader = DataLoader(validation_set, batch_size=batch_size, shuffle=True, num_workers=1)
 
     model = PlayerPredictor(device, n_nearest_players, 32, 4)
     model.to(device)
-
-    learning_rate = 1e-4
-    epochs = 5
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    epochs = 20
     loss_function = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
