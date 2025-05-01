7c58b0bb9c13daa93e21957fa20cdf70693d7f79
MathiasFM <mathiasmogensen1@gmail.com>
2025-05-01 12:07:20 +0200

    add test for swap players

--- services/master/detector/detector_test.go
+++ services/master/detector/detector_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestPositionProximity(t *testing.T) {
-    // Test cases
     testCases := []struct {
         a, b     swapPlayer
         expected bool
@@ -25,5 +24,33 @@ func TestPositionProximity(t *testing.T) {
     for _, tc := range testCases {
         result := positionProximity(tc.a, tc.b)
         assert.Equal(t, tc.expected, result)
+    }
+}
+
+func TestSwapPlayers(t *testing.T) {
+    testCases := []struct {
+        p                    map[string]types.PlayerPosition
+        p1, p2               swapPlayer
+        expectedA, expectedB types.Position
+    }{
+        {
+            p: map[string]types.PlayerPosition{
+                "player1": {Position: types.Position{X: 3.0, Y: 4.0}},
+                "player2": {Position: types.Position{X: 1.0, Y: 2.0}}},
+            p1: swapPlayer{
+                key: "player1",
+                player: types.PlayerPosition{
+                    Position: types.Position{X: 3.0, Y: 4.0}}},
+            p2: swapPlayer{
+                key: "player2",
+                player: types.PlayerPosition{
+                    Position: types.Position{X: 1.0, Y: 2.0}}},
+            expectedA: types.Position{X: 1.0, Y: 2.0},
+            expectedB: types.Position{X: 3.0, Y: 4.0}},
+    }
+    for _, tc := range testCases {
+        swapPlayers(tc.p, tc.p1, tc.p2)
+        assert.Equal(t, tc.expectedA, tc.p[tc.p1.key].Position)
+        assert.Equal(t, tc.expectedB, tc.p[tc.p2.key].Position)
     }
 }
