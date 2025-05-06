382f4f2deba16e44c3f8bd6aa9f102da1639df3f
MathiasFM <mathiasmogensen1@gmail.com>
2025-05-06 12:38:41 +0200

    test swaps

--- services/master/detector/detector_test.go
+++ services/master/detector/detector_test.go
@@ -2,12 +2,82 @@ package detector
 
 import (
     "github.com/Ygg-Drasill/DookieFilter/common/types"
+    "github.com/Ygg-Drasill/DookieFilter/services/master/worker"
     "github.com/stretchr/testify/assert"
     "testing"
 )
 
-func TestSwap(t *testing.T) {
+type MockWorker struct {
+    *Worker
+}
 
+func getMockWorker() *MockWorker {
+    return &MockWorker{
+        Worker: &Worker{
+            BaseWorker: worker.NewBaseWorker(
+                nil,
+                "detector"),
+        },
+    }
+}
+
+func (m *MockWorker) setupMockData() map[string]types.PlayerPosition {
+    return map[string]types.PlayerPosition{
+        "player1:10:0": {PlayerId: "player1", FrameIdx: 10, Position: types.Position{X: 3.0, Y: 4.0}},
+        "player1:11:1": {PlayerId: "player1", FrameIdx: 11, Position: types.Position{X: 5.05, Y: 6.04}},
+        "player2:10:0": {PlayerId: "player2", FrameIdx: 10, Position: types.Position{X: 5.0, Y: 6.0}},
+        "player2:11:1": {PlayerId: "player2", FrameIdx: 11, Position: types.Position{X: 3.02, Y: 4.04}},
+    }
+}
+
+func TestWorkerSwap(t *testing.T) {
+    testCases := []struct {
+        name       string
+        p          map[string]types.PlayerPosition
+        expectedXY map[string]types.Position
+    }{
+        {
+            name: "Test Case 1",
+            p: map[string]types.PlayerPosition{
+                "player1:10:0": {PlayerId: "player1", FrameIdx: 10, Position: types.Position{X: 3.0, Y: 4.0}},
+                "player1:11:1": {PlayerId: "player1", FrameIdx: 11, Position: types.Position{X: 5.05, Y: 6.04}},
+                "player2:10:0": {PlayerId: "player2", FrameIdx: 10, Position: types.Position{X: 5.0, Y: 6.0}},
+                "player2:11:1": {PlayerId: "player2", FrameIdx: 11, Position: types.Position{X: 3.02, Y: 4.04}},
+            },
+            expectedXY: map[string]types.Position{
+                "player1:11:1": {X: 3.02, Y: 4.04},
+                "player2:11:1": {X: 5.05, Y: 6.04},
+            },
+        },
+        {
+            name: "Test Case 2",
+            p: map[string]types.PlayerPosition{
+                "player1:10:0": {PlayerId: "player1", FrameIdx: 10, Position: types.Position{X: 3.0, Y: 4.0}},
+                "player1:11:1": {PlayerId: "player1", FrameIdx: 11, Position: types.Position{X: 5.05, Y: 6.04}},
+                "player2:10:0": {PlayerId: "player2", FrameIdx: 0, Position: types.Position{X: 0.0, Y: 0.0}},
+                "player2:11:1": {PlayerId: "player2", FrameIdx: 11, Position: types.Position{X: 3.02, Y: 4.04}},
+            },
+            expectedXY: map[string]types.Position{
+                "player1:11:1": {X: 3.02, Y: 4.04},
+                "player2:11:1": {X: 5.05, Y: 6.04},
+            },
+        },
+    }
+    for _, tc := range testCases {
+        t.Run(tc.name, func(t *testing.T) {
+
+            mockWorker := getMockWorker()
+            mockWorker.swap(true, tc.p)
+
+            for key, expectedPos := range tc.expectedXY {
+                if player, exists := tc.p[key]; exists {
+                    assert.Equal(t, expectedPos, player.Position)
+                } else {
+                    t.Errorf("Player %s not found in map", key)
+                }
+            }
+        })
+    }
 }
 
 func TestPositionProximity(t *testing.T) {
