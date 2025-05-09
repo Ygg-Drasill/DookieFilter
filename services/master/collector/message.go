207837abb8ebd4b8b7c127a5407fc01c180dc0dc
Tobias Bay <tbay22@student.aau.dk>
2025-05-09 14:16:14 +0200

    added homeplayer in struct

--- services/master/collector/message.go
+++ services/master/collector/message.go
@@ -1,6 +1,7 @@
 package collector
 
 type holeMessage struct {
-	FrameIdx     int `json:"frameIdx"`
-	PlayerNumber int `json:"playerNumber"`
+	FrameIdx     int  `json:"frameIdx"`
+	PlayerNumber int  `json:"playerNumber"`
+	HomePlayer   bool `json:"homePlayer"`
 }
