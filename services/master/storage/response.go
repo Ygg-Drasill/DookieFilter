f77f0f9a65566dac5990a47db6f1d4492bbbf178
alexrefshauge <nk02hb@aau.student.dk>
2025-05-16 13:32:59 +0200

    find nearest playerse

--- services/master/storage/response.go
+++ services/master/storage/response.go
@@ -1,1 +1,11 @@
 package storage
+
+import "github.com/Ygg-Drasill/DookieFilter/common/types"
+
+type FrameNearest struct {
+	Target types.PlayerPosition `json:"target"`
+	Home   playerDistanceSorted `json:"home"`
+	Away   playerDistanceSorted `json:"away"`
+}
+
+type FrameRangeNearestResponse []FrameNearest
--- services/master/storage/sort.go
+++ services/master/storage/sort.go
@@ -1,1 +1,22 @@
 package storage
+
+import "github.com/Ygg-Drasill/DookieFilter/common/types"
+
+type playerDistance struct {
+	types.PlayerPosition
+	distance float64
+}
+
+type playerDistanceSorted []playerDistance
+
+func (pd playerDistanceSorted) Len() int {
+	return len(pd)
+}
+
+func (pd playerDistanceSorted) Less(i, j int) bool {
+	return pd[i].distance < pd[j].distance
+}
+
+func (pd playerDistanceSorted) Swap(i, j int) {
+	pd[i], pd[j] = pd[j], pd[i]
+}
