39178d52763789707582527a1a242c02e0f0cb9b
alexrefshauge <nk02hb@aau.student.dk>
2025-05-22 21:13:29 +0200

    implement filterable frame

--- services/master/filter/element.go
+++ services/master/filter/element.go
@@ -4,22 +4,44 @@ import (
 	"fmt"
 	"github.com/Ygg-Drasill/DookieFilter/common/filter"
 	"github.com/Ygg-Drasill/DookieFilter/common/types"
+	"strconv"
+	"strings"
 )
 
 type filterableFrame types.SmallFrame
 
-func (f filterableFrame) Update(key string, value float64) error {
-	//TODO implement me
-	panic("implement me")
+func (f *filterableFrame) Update(key string, value float64) error {
+	h, n, x := decodeKey(key)
+	for i, player := range f.Players {
+		if player.Home == h && player.PlayerNum == n {
+			if x {
+				f.Players[i].X = value
+			} else {
+				f.Players[i].Y = value
+			}
+		}
+	}
+
+	return filter.KeyNotFoundError{}
 }
 
-func (f filterableFrame) Get(key string) (float64, error) {
-	//TODO implement me
-	panic("implement me")
+func (f *filterableFrame) Get(key string) (float64, error) {
+	h, n, x := decodeKey(key)
+	for _, player := range f.Players {
+		if player.Home == h && player.PlayerNum == n {
+			if x {
+				return player.X, nil
+			} else {
+				return player.Y, nil
+			}
+		}
+	}
+
+	return 0, filter.KeyNotFoundError{}
 }
 
 type savGolFilter struct {
-	filter.Filter[filterableFrame]
+	filter.Filter[*filterableFrame]
 }
 
 func (f savGolFilter) Keys() []string {
@@ -31,4 +53,12 @@ func (f savGolFilter) Keys() []string {
 	}
 
 	return keys
+}
+
+func decodeKey(key string) (bool, int, bool) {
+	parts := strings.Split(key, "_")
+	home, _ := strconv.ParseBool(parts[0])
+	number, _ := strconv.ParseInt(parts[1], 10, 64)
+	axis := parts[2] == "x"
+	return home, int(number), axis
 }
